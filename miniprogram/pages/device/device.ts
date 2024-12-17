72964d984e86e3e39c5342f0245bba89b5e98ae1
odd douglas <113650927+odddouglas@users.noreply.github.com>
2024-12-17 16:12:41 +0800

    build：基本建立小程序原生框架
    已经完成BLE功能模块、echart功能模块的调试，建立基本框架，包含home、device两个页面

---
+++ miniprogram/pages/device/device.ts
@@ -0,0 +1,55 @@
+
+import * as echarts from "../../utils/ec-canvas/echarts";
+const app = getApp();
+const bluetooth = require('../../module/bluetooth.js'); // Import the new Bluetooth module
+const echart = require('../../module/echarts.js');
+
+Page({
+
+  data: {
+
+  },
+
+
+
+  //蓝牙模块函数调用 
+  openBluetoothAdapter() {
+    bluetooth.openBluetoothAdapter(this);
+  },
+  createBLEConnection(e) {
+    bluetooth.createBLEConnection(this, e);
+  },
+  closeBLEConnection() {
+    bluetooth.closeBLEConnection(this);
+  },
+
+  //生命周期函数
+  onLoad() {
+
+  },
+
+  onReady() {
+
+  },
+
+  onShow() {
+
+  },
+
+  onHide() {
+
+  },
+
+  onUnload() {
+
+  },
+
+  onPullDownRefresh() {
+
+  },
+
+  onReachBottom() {
+
+  },
+
+})
