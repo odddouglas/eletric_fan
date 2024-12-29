63a6f26f977492b97567e52484e69ec874fb6c2f
odddouglas <18718048671iota@gmail.com>
2024-12-29 19:22:15 +0800

    feat: 折线图无数据显示

--- wx_miniprogram/miniprogram/module/myecharts.js
+++ wx_miniprogram/miniprogram/module/myecharts.js
@@ -107,7 +107,28 @@ function initChart(getOptionFunc, canvas, width, height, dpr) {
 //   };
 // }
 
-function getOption1(data) {
+function getOption1() {
+  // 生成随机数据
+  function randomData() {
+    now = new Date(+now + oneDay);
+    value = value + Math.random() * 21 - 10;
+    return {
+      name: now.toString(),
+      value: [
+        [now.getFullYear(), now.getMonth() + 1, now.getDate()].join('/'),
+        Math.round(value)
+      ]
+    };
+  }
+
+  let data = [];
+  let now = new Date(1997, 9, 3);
+  let oneDay = 24 * 3600 * 1000;
+  let value = Math.random() * 1000;
+  for (var i = 0; i < 1000; i++) {
+    data.push(randomData());
+  }
+
   return {
     title: {
       text: 'Dynamic Data & Time Axis'
@@ -152,7 +173,6 @@ function getOption1(data) {
     }]
   };
 }
-
 
 
 //加载条
